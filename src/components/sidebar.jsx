8ca4960ca7b3168eb4757212f5dd1c404c433c35
Hui Ming Tey <huimingtey@u.nus.edu>
2023-08-15 17:34:47 +0800

    Add api call to providers using axios

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './sidebar.css';
 
-const dummyData = [
+/* const dummyData = [
     "1forge.com",
     "1password.com",
     "1password.local",
     "6-dot-authentiqio.appspot.com",
     "ably.io",
     "ably.net"
-]
+] */
 
 function Sidebar() {
+    const [providers, setProviders] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            let providerData = await axios.get('https://api.apis.guru/v2/providers.json');
+            setProviders(providerData.data.data);
+        }
+        
+        fetchData();
+    }, [])
+
     return (
         <div id="sidebar">
             Select Provider
-            {dummyData.map(provider => {
+            {providers.map(provider => {
                 return (
                     <div>
                         {provider}<i class="arrow"></i>
